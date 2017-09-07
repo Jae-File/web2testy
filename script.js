0724dcc8f0c9f5624279a6677ed78d9200c6b2f6
Yajie Zhang <J.Key.Noble@gmail.com>
2017-09-07 16:36:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 console.log('hello Jae!')
 let adj_list = ['shiny', 'funky', 'amazing']
-'Last week is a '+ adj_list[Math.floor(adjectives.length*Math.random())] + 'week, '
-document.write()
+let sentence='Last week is a '+ adj_list[Math.floor(adj_list.length*Math.random())] + ' week, '
+document.write(sentence)
