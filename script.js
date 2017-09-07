3634f383ec5e8d45920963ced1894b150ffbe17a
Yajie Zhang <J.Key.Noble@gmail.com>
2017-09-07 17:45:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log('hello Jae!');
 let adj_list = ['shiny', 'funky', 'amazing'];
 let adj=adj_list[Math.floor(adj_list.length*Math.random())];
-let var noun_list = prompt("Enter 6 nouns, separate them by comma").split(",");
+var noun_list = prompt("Enter 6 nouns, separate them by comma").split(",");
 let noun=noun_list[Math.floor(noun_list.length*Math.random())];
 let sentence='Last week is a '+ adj + ' ' + noun;
 document.write(sentence);
