5edba1c1f946c5f529be049914402787a625a607
Yajie Zhang <J.Key.Noble@gmail.com>
2017-09-08 16:23:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ let verb_list = ['went', 'ran', 'was', 'jumped', 'saw', 'kicked'];
 let verb=verb_list[Math.floor(verb_list.length*Math.random())];
 var noun_list = prompt("Enter 5 nouns, separate them by comma").split(",");
 let noun=noun_list[Math.floor(noun_list.length*Math.random())];
-let sentence='Last Sunday '+verb+ ' a '+ adj + ' ' + noun +'. On my way to the '+ noun + 'I '+verb +' a very ' + adj + noun + '. It '+verb+' so ' + adj + ' that I instantly '+ verb + ' over it. Later at night, my ' + noun + verb + ' to my '+ noun + ' and together we '+ verb + ' up, and ' + verb + ' some '+noun ;
+let sentence='Last Sunday I'+verb+ ' a '+ adj + ' ' + noun +'. On my way to the '+ noun + 'I '+verb +' a very ' + adj +' '+ noun + '. It '+verb+' so ' + adj + ' that I instantly '+ verb + ' over it. Later at night, my ' + noun + verb + ' to my '+ noun + ' and together we '+ verb + ' up, and ' + verb + ' some '+noun ;
 document.write(sentence);
