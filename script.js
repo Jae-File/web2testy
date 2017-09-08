08dc4ecb8ac38e77589d890bdf89192956ee0c5c
Yajie Zhang <J.Key.Noble@gmail.com>
2017-09-08 16:20:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 console.log('hello Jae!');
-let adj_list = ['shiny', 'funky', 'amazing'];
+let adj_list = ['shiny', 'funky', 'amazing', 'tasty', 'tiny'];
 let adj=adj_list[Math.floor(adj_list.length*Math.random())];
-var noun_list = prompt("Enter 6 nouns, separate them by comma").split(",");
+let verb_list = ['went', 'ran', 'was', 'jumped', 'saw', 'kicked'];
+let verb=verb_list[Math.floor(verb_list.length*Math.random())];
+var noun_list = prompt("Enter 5 nouns, separate them by comma").split(",");
 let noun=noun_list[Math.floor(noun_list.length*Math.random())];
-let sentence='Last week is a '+ adj + ' ' + noun;
+let sentence='Last Sunday '+verb+ ' a '+ adj + ' ' + noun +'. On my way to the '+ noun + 'I '+verb +' a very ' + adj + noun + '. It '+verb+' so ' + adj + ' that I instantly '+ verb + ' over it. Later at night, my ' + noun + verb + ' to my '+ noun + ' and together we '+ verb + ' up, and ' + verb + ' some '+noun ;
 document.write(sentence);
