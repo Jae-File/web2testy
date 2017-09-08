6a1d62d29d66072bfee85b4d47705798b4bde0fd
Yajie Zhang <J.Key.Noble@gmail.com>
2017-09-08 17:19:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 console.log('hello Jae!');
-var adj_list = ['shiny', 'funky', 'amazing', 'tasty', 'tiny'];
-var adj=adj_list[Math.floor(adj_list.length*Math.random())];
-var verb_list = ['went', 'ran', 'was', 'jumped', 'saw', 'kicked'];
-var verb=verb_list[Math.floor(verb_list.length*Math.random())];
-var noun_list = prompt("Enter 5 nouns, separate them by comma").split(",");
-var noun=noun_list[Math.floor(noun_list.length*Math.random())];
-var sentence='Last Sunday I '+verb+ ' a '+ adj + ' ' + noun +'. On my way to the '+ noun + ', I '+verb +' a very ' + adj +' '+ noun + '. It '+verb+' so ' + adj + ' that I instantly '+ verb + ' over it. Later at night, my ' + noun +' '+ verb + ' to my '+ noun + ', and together we '+ verb + ' up, and ' + verb + ' some '+noun ;
+let adj_list = ['shiny', 'funky', 'amazing', 'tasty', 'tiny'];
+let verb_list = ['went', 'ran', 'was', 'jumped', 'saw', 'kicked'];
+let noun_list = prompt("Enter 5 nouns, separate them by comma").split(",");
+let sentence='Last Sunday I '+verb_list[Math.floor(verb_list.length*Math.random())]+ ' a '+ adj_list[Math.floor(adj_list.length*Math.random())] + ' ' + noun_list[Math.floor(noun_list.length*Math.random())] +'. On my way to the '+ noun_list[Math.floor(noun_list.length*Math.random())] + ', I '+verb_list[Math.floor(verb_list.length*Math.random())] +' a very ' + adj_list[Math.floor(adj_list.length*Math.random())] +' '+ noun_list[Math.floor(noun_list.length*Math.random())] + '. It '+verb_list[Math.floor(verb_list.length*Math.random())]+' so ' + adj_list[Math.floor(adj_list.length*Math.random())] + ' that I instantly '+ verb_list[Math.floor(verb_list.length*Math.random())] + ' over it. Later at night, my ' + noun_list[Math.floor(noun_list.length*Math.random())] +' '+ verb_list[Math.floor(verb_list.length*Math.random())] + ' to my '+ noun_list[Math.floor(noun_list.length*Math.random())] + ', and together we '+ verb_list[Math.floor(verb_list.length*Math.random())] + ' up, and ' + verb_list[Math.floor(verb_list.length*Math.random())] + ' some '+noun_list[Math.floor(noun_list.length*Math.random())] ;
 document.write(sentence);
